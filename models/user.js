35e07b30f80c74b829a0292d2ddebcd5235ca599
Carlos S. <carlossalmeroncabrera@gmail.com>
2024-06-07 22:22:45 -0600

    Added user mongoose model

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  username: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  password: { type: String, required: true },
+  membership: { type: String, required: true },
+});
+
+UserSchema.virtual("full_name").get(function () {
+  return `${this.first_name} ${this.last_name}`;
+});
+
+UserSchema.virtual("url").get(function () {
+  return `/catalog/category/${this._id}`;
+});
+
+module.exports = mongoose.model("User", UserSchema);
